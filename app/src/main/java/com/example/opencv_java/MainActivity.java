1ad437fddc0c38dd87d3791130e274db971b5638
shrayan <shrayan@pathway.com.np>
2022-04-12 17:01:22 +0545

    First commit

---
+++ app/src/main/java/com/example/opencv_java/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.opencv_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+
+        if (OpenCVLoader.initDebug()) {
+
+            Log.d("Check", "OpenCv configured successfully");
+
+        } else {
+
+            Log.d("Check", "OpenCv doesn’t configured successfully");
+
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
